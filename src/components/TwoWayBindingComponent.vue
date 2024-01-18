ff383158c5feec5294df7246f8bf62e35530dfe9
10038-machicaiza17 <132980301+10038-machicaiza17@users.noreply.github.com>
2024-01-17 22:39:01 -0500

    Uploading Files

---
+++ src/components/TwoWayBindingComponent.vue
@@ -0,0 +1,32 @@
+<!-- TwoWayBindingComponent.vue -->
+
+<template>
+  <div>
+    <input v-model="message" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Inicializar el mensaje aquí',
+    };
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 50px;
+  color: green;
+}
+
+input{
+  width: 400px;
+  height: 15px;
+  font-size: larger;
+}
+
+</style>
